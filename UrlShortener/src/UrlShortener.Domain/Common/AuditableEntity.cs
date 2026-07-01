1cab4d4aee4594fe45ef26b295526d05d57b54cb
Chintan <chhapgarchintan@yahoo.com>
2026-07-01 14:36:21 +0530

    Add domain entities and base classes

---
+++ UrlShortener/src/UrlShortener.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Domain.Common;
+
+public abstract class AuditableEntity : BaseEntity
+{
+    public DateTime CreatedOnUtc { get; set; } = DateTime.UtcNow;
+
+    public DateTime? ModifiedOnUtc { get; set; }
+}
---
+++ UrlShortener/src/UrlShortener.Domain/Entities/ShortUrl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.Domain.Common;
+
+namespace UrlShortener.Domain.Entities;
+
+public class ShortUrl : AuditableEntity
+{
+    public string OriginalUrl { get; private set; } = string.Empty;
+
+    public string ShortCode { get; private set; } = string.Empty;
+
+    public int ClickCount { get; private set; }
+
+    private ShortUrl()
+    {
+    }
+
+    public ShortUrl(string originalUrl, string shortCode)
+    {
+        OriginalUrl = originalUrl;
+        ShortCode = shortCode;
+    }
+
+    public void RegisterClick()
+    {
+        ClickCount++;
+    }
+}
